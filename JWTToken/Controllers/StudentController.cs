985f0cc3ff3c88576d87e0fb188898acb8618686
Shirisha091 <shirisha.s@innodigital.in>
2025-03-28 11:01:46 +0530

    Add project files.

---
+++ JWTToken/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using JWTToken.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return repository.students;
+
+        }
+        [HttpGet("{id}")]
+        public Student GetStudentsbyId(int id)
+        {
+            return repository.students.Where(n => n.Id == id).FirstOrDefault();
+
+        }
+    }
+}
---
+++ JWTToken/Model/repository.cs
@@ -0,0 +1,30 @@
+﻿namespace JWTToken.Model
+{
+    public static class repository
+    {
+        public static List<Student> students { get; set; } = new List<Student>()
+        {
+            new Student
+                {
+                    Id = 1,
+                    Name = "John",
+                    Email = "john@gmail.com",
+                    Address = "USA"
+                },
+                new Student
+                {
+                    Id = 2,
+                    Name = "Jane",
+                    Email = "jane@gmail.com",
+                    Address = "Canada"
+                },
+                 new Student
+                {
+                    Id = 3,
+                    Name = "Jane",
+                    Email = "jane@gmail.com",
+                    Address = "Canada"
+                }
+        };
+    }
+}
